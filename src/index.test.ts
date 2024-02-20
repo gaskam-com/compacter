36fcd4880e361b6af91191db8cd9aca4a4f56ae6
PatafixPLTX <gaspard.lebaube@gaskam.com>
2024-02-20 23:24:37 +0100

    Add tests for html minifier function

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,11 +3,17 @@ import { rewrite } from ".";
 
 const input = "index.html";
 describe("Test html minifier function", () => {
+  test("2 * 2", () => {
+    expect(2 * 2).toBe(4);
+  });
+
   test("Expect minified to be smaller than original", async () => {
     expect((await rewrite(input)).length).toBeLessThan((await Bun.file("test/src/" + input).text()).length);
   });
 
-  test("2 * 2", () => {
-    expect(2 * 2).toBe(4);
+  test("Expect minified to be 40% smaller than original", async () => {
+    const original = (await Bun.file("test/src/" + input).text()).length;
+    const minified = (await rewrite(input)).length;
+    expect(minified).toBeLessThan(original * 0.6);
   });
 });
